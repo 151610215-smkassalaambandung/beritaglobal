3a388e208f4694a5b875339e95180d73d5b1fafc
smk-141510262 <141510262@smkassalaambandung.sch.id>
2017-07-18 12:35:04 +0700

    berita

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<br>
+@include('layouts.slide')
+</br>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel" style="background: rgba(255,255,255,0,5);">
+                <div class="Panel-heading">Berita</div>
+                <div class="Panel-body">
+                    Selamat Datang
+                </div>
+            </div>
+        </div> 
+    </div>
+</div>
+@endsection
